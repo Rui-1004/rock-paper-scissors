61080813eeceb6679b4877617fac12ba0573e770
Rui <77292122+Rui-1004@users.noreply.github.com>
2024-03-19 17:16:13 +0000

    Add logic to have five rounds instead of one Add logic to keep track of score Add logic to declare a final winner
    A for loop was used to play five consecutive rounds and display a winner at the end.
    Scores were added and kept track of. After the five rounds a winner or a draw is declared, after comparing the scores.

--- play.js
+++ play.js
@@ -11,15 +11,44 @@ function playRound(playerSelection, computerSelection) {
   }
   // Player win:
   else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "paper") {
+    playerScore++;
     return `You Win! ${playerSelection} wins against ${computerSelection}`;
   }
   // Player loss:
   else {
+    computerScore++;
     return `You Lose! ${playerSelection} loses against ${computerSelection}`;
   }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
+function playGame() {
+  // Play a five round game
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = prompt("Choose between rock, paper and scissors.").toLowerCase();
+    const computerSelection = getComputerChoice();
 
-console.log(playRound(playerSelection, computerSelection));
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(`Current score: Player - ${playerScore}, Computer - ${computerScore}`);
+  }
+
+  // Compare final scores
+  if (playerScore > computerScore) {
+    return console.log(`Player wins! Final score: Player - ${playerScore}, Computer - ${computerScore}`);
+  }
+  else if (playerScore === computerScore) {
+    return console.log(`Draw! Final score: Player - ${playerScore}, Computer - ${computerScore}`);
+  }
+  else {
+    return console.log(`Computer wins! Final score: Player - ${playerScore}, Computer - ${computerScore}`);
+  }
+}
+
+let playerScore = 0;
+let computerScore = 0;
+
+playGame();
+
+
+
+
+
